2a7a98350089f2e1a35e55fd1a14fe28c4bbc44f
fuzzy <ahmed.faiyaz1532@gmail.com>
2024-11-19 11:04:13 +0600

    types basics & enums

---
+++ basics_of_typescripts/src/typescript_array.ts
@@ -0,0 +1,5 @@
+const numbers:number[] = [1034,492432,2929]/// annotate type by number[]
+console.log(numbers)
+
+const mixed:[number,string]=[1000,"Something"] /// define multiple type
+console.log(mixed)
