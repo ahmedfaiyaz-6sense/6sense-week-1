ed759e570ac73c354bfe2ad5d77e8dc9963db91a
fuzzy <ahmed.faiyaz1532@gmail.com>
2024-11-19 11:08:44 +0600

    seperated js files

--- basics_of_typescripts/src/enum.js
+++
@@ -1,8 +0,0 @@
-var Size;
-(function (Size) {
-    Size[Size["small"] = 0] = "small";
-    Size[Size["large"] = 1] = "large";
-    Size[Size["larger"] = 2] = "larger";
-})(Size || (Size = {}));
-var shirtSize = Size.larger;
-console.log(shirtSize);
--- basics_of_typescripts/src/types_basics.js
+++
@@ -1,7 +0,0 @@
-/// javascript types are
-//// ---> number,string,boolean,null,undefined,object
-// typescript extends this by
-/// any,unknown,never,enum,tuple
-var sales = 10000;
-var course = "New Course";
-var level; // this is type 'any'
