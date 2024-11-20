3e4c87af1beb10583349b15731ae05c7d8c1dddd
fuzzy <ahmed.faiyaz1532@gmail.com>
2024-11-20 19:02:05 +0600

    added all for the day

--- uptime_monitoring_1/handlers/routeHandlers/userHandler.js
+++ uptime_monitoring_1/handlers/routeHandlers/userHandler.js
@@ -51,54 +51,40 @@ handler.user.post = (reqProps,callback)=>{
     }
 }
 handler.user.put = (reqProps,callback)=>{
-    const firstName = typeof(reqProps?.queryStringObject?.firstName) === 'string' && reqProps.queryStringObject?.firstName?.trim().length>0 ? reqProps?.queryStringObject?.firstName : false
-    const lastName = typeof(reqProps?.queryStringObject?.lastName) === 'string' && reqProps.queryStringObject?.lastName?.trim().length>0 ? reqProps?.queryStringObject?.lastName : false
-    const mobile_number= typeof(reqProps?.queryStringObject?.mobile_number) === 'string' && reqProps?.queryStringObject?.mobile_number.trim().length>0 ? reqProps?.queryStringObject?.mobile_number : false
-    const password= typeof(reqProps?.queryStringObject?.password) === 'string' && reqProps?.queryStringObject?.password.trim().length>0 ? reqProps?.queryStringObject?.password : false
-
-    if (firstName || lastName || mobile_number || password){
-        //console.log("USER OBJECT: ")
-        
-        //console.log(userObject)
-        data.read('user',mobile_number,(err,userInfo)=>{
-            console.log(userInfo)
-            if(err){
-                callback(500,{
-                    message:'Internal Server error'
+    const phone = typeof reqProps.queryStringObject?.mobile_number === 'string' && reqProps.queryStringObject?.mobile_number?.trim() ? reqProps.queryStringObject?.mobile_number : false
+    console.log("PUT")
+    data.read('user',phone,(err,userInfo)=>{
+        if(!err && d){
+            const firstName = typeof(reqProps?.body?.firstName) === 'string' && reqProps.body?.firstName?.trim().length>0 ? reqProps?.body?.firstName : false
+            const lastName = typeof(reqProps?.body?.lastName) === 'string' && reqProps.body?.lastName?.trim().length>0 ? reqProps?.body?.lastName : false
+            const mobile_number= typeof(reqProps?.body?.mobile_number) === 'string' && reqProps?.body?.mobile_number.trim().length>0 ? reqProps?.body?.mobile_number : false
+            const password= typeof(reqProps?.body?.password) === 'string' && reqProps?.body?.password.trim().length>0 ? reqProps?.body?.password : false
+            if (firstName || lastName || mobile_number || password){ 
+                console.log("One or more field needs to be updated")
+                data.update('user',phone,userInfo,(err)=>{
+                    if(err){
+                        callback(500,{
+                            'message':'Internal server error'
+                        })
+                    }else{
+                        if(userInfo.firstName)
+                            userInfo.firstName=firstName
+                        if(userInfo.lastName)
+                            userInfo.lastName=lastName
+                        if(userInfo.mobile_number)
+                            userInfo.mobile_number=mobile_number
+                        if(userInfo.password)
+                            userInfo.password=password
+                    }
+                    callback(201,{'message':'User updated'})
                 })
             }else{
-                console.log(userInfo)
-                if(firstName){
-                    userInfo.firstName=firstName
-                }
-                if(lastName){
-                    userInfo.lastName=lastName
-                }
-                if(mobile_number){
-                    userInfo.mobile_number=mobile_number
-                }
-                if(password){
-                    userInfo.password=password
-                }
-                data.update('user',mobile_number,userInfo,(err)=>{
-                    if(err){
-                        callback(500,{user:"User not updated"})
-                    }
-                    else{
-                        callback(201,{user:"user updated"})
-                    }
-                })
-                callback(201,{
-                    success:'True.',
-                    message:'User created'
+                callback(500,{
+                    'message':'Invalid fields'
                 })
             }
-        })
-    }else{
-        callback(404,{
-            message:"Invalid Fields"
-        })
-    }
+        }
+    })  
 }
 handler.user.get = (reqProps,callback)=>{
     console.log(reqProps)
