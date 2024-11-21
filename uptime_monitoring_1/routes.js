8957fac2c6ed23164340c60bccdf30cc6d83f4ba
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-11-21 08:20:09 +0600

    added token handler

--- uptime_monitoring_1/routes.js
+++ uptime_monitoring_1/routes.js
@@ -1,10 +1,12 @@
 const sampleHandler=require('./handlers/routeHandlers/sampleHandler')
 const userHandler=require('./handlers/routeHandlers/userHandler')
+const tokenHandler = require('./handlers/routeHandlers/tokenHandler')
 const routes={
 
 /// here functionalities with proper name will be assigned like 'login':loginHandler
     sample:sampleHandler,
-    user:userHandler
+    user:userHandler,
+    token:tokenHandler,
 }
 
 module.exports=routes
