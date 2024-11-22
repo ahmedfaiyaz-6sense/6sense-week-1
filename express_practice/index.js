ab3ddfbae082f9f94373b50944823ee7bd765cd4
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-11-22 11:36:39 +0600

    error handling

--- express_practice/index.js
+++ express_practice/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs= require('node:fs')
 //const dashboard = require('./dashboard_api')
 const {adminRouter,publicRouter}=require('./routing_example')
 const app= express()
@@ -8,8 +9,40 @@ app.get('/',(req,res)=>{
     res.send('Express is running.')
 })
 
+
+
+
 app.use('/admin_router',adminRouter)
 app.use('/public_router',publicRouter)
+
+// error handler 
+/// middleware chaining to handle error
+app.get('/chaining',[
+    (req,res,next)=>{
+        fs.readFile('./somefile.txt','utf-8',(err,data)=>{
+            if(err){
+                next(err)
+            }else{
+                console.log("File read successfull")
+            }
+        })
+    },
+    (req,res,next)=>{
+        console.log(data)
+    }
+])
+//404 handler
+app.use((req,res,next)=>{
+    res.send("Request Not found")
+})
+app.use((err,req,res,next)=>{
+    if(err.message){
+        res.status(500).send(err.message)
+    }else{
+        res.status(500).send(err.message)
+    }
+})
+
 app.listen(3000,()=>{
     console.log("Listening to port: "+3000)
 })
