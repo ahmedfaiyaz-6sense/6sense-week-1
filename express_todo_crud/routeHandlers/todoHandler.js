ac417eb1251905bd1d9b432d9ee171d9a99b5013
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-11-22 16:40:04 +0600

    added todo app api mongodb backend

---
+++ express_todo_crud/routeHandlers/todoHandler.js
@@ -0,0 +1,123 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const todo_route = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+//console.log(todoSchema)
+const Todo = new mongoose.model("Todo", todoSchema);
+//get all the todos
+todo_route.get("/", async (req, res) => {
+  res.send({
+    message: "Get request",
+  });
+});
+
+//get a todo by id
+todo_route.get("/:id", async (req, res) => {
+  await Todo.findByIdAndUpdate(
+    { _id: req.params.id },
+
+  )
+    .then((data) => {
+      if (data) {
+        res.send({
+          message: "Updated",
+          mongo_message: data,
+        });
+      } else {
+        res.send({
+          message: "Record not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.send({
+        message: "Error Not a valid id",
+      });
+    });
+});
+
+//post todo
+todo_route.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo.save().then((data) => {
+    res.send({
+      message: "Note insertion successful",
+      mongo_message:data,
+    });
+  }).catch((error)=>{
+    res.send({
+      message:error
+    })
+  });
+});
+
+//post multiple todo
+todo_route.post("/all", async (req, res) => {
+  //console.log(req.body)
+  await Todo.insertMany(req.body)
+    .then((data) => {
+      res.send({
+        message: "Notes inserted.",
+        mongo_message: data,
+      });
+    })
+    .catch((err) => {
+      res.send({
+        error: "Error",
+      });
+    });
+});
+
+//put todo
+todo_route.put("/:id", async (req, res) => {
+  await Todo.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: "active",
+      },
+    }
+  )
+    .then((data) => {
+      res.send({
+        message: "Updated",
+        mongo_message: data,
+      });
+    })
+    .catch((error) => {
+      res.send({
+        message: "Error",
+      });
+    });
+});
+
+//post delete
+todo_route.delete("/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: "active",
+      },
+    }
+  )
+    .then((data) => {
+      if (data) {
+        res.send({
+          message: "Deleted",
+          mongo_message: data,
+        });
+      } else {
+        res.send({
+          message: "No record found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.send({
+        message: "Error",
+      });
+    });
+});
+
+module.exports = todo_route;
