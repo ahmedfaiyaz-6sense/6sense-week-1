0cdb933d8eaa75de4ed237a5bdbec88610b2f5db
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-11-22 11:13:41 +0600

    added routing examples

--- express_practice/index.js
+++ express_practice/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 //const dashboard = require('./dashboard_api')
-const routing_example=require('./routing_example')
+const {adminRouter,publicRouter}=require('./routing_example')
 const app= express()
 app.use(express.json())
 
@@ -8,8 +8,8 @@ app.get('/',(req,res)=>{
     res.send('Express is running.')
 })
 
-app.use('/routing_example',routing_example)
-
+app.use('/admin_router',adminRouter)
+app.use('/public_router',publicRouter)
 app.listen(3000,()=>{
     console.log("Listening to port: "+3000)
 })
--- express_practice/routing_example.js
+++ express_practice/routing_example.js
@@ -1,9 +1,69 @@
 const express = require("express");
-const router = express.Router();
+const adminRouter = express.Router();
+const publicRouter = express.Router()
 
-
-router.get('/',(req,res)=>{
-    res.send("Routing working")
+adminRouter.get('/',(req,res)=>{
+    res.send({
+        'panel':"Admin Panel",
+        "stats":{
+            "amount":20,
+            "profit":"+30",
+        }
+    })
 })
 
-module.exports=router
+
+
+/*public router */
+const log =(req,res,next)=>{
+    console.log("Logging- Base URL: "+req.baseUrl+" original URL: "+req.originalUrl+" Method: "+req.method)
+    
+    next()
+}
+
+
+//all functon
+publicRouter.all('*',log)
+/// req param
+
+/*publicRouter.param((param,options)=>(req,res,next,val)=>{
+    if(val==options){
+        next()
+    }else{
+        res.sendStatus(403)
+    }
+})
+publicRouter.param('user',1)*/
+publicRouter.param('user',(req,res,next,id)=>{
+    req.user = id==='1'? 'Admin':'user'
+    next()
+})
+
+
+///router.route method
+publicRouter.route('/shop').all((req,res,next)=>{
+    console.log("Logging user")
+    next()
+}).get((req,res)=>{
+    res.send("GET")
+}).post((req,res)=>{
+    res.send("POST")
+}).put((req,res)=>{
+    res.send("PUT")
+})
+
+
+// Ger request if /:user = matches /1 ---> /999999 then pass it to param with id param takes `user` as input
+publicRouter.get('/:user',(req,res)=>{
+    res.send("Designation: "+req.user)
+})
+publicRouter.get('/',(req,res)=>{
+    res.send("Public Router")
+})
+
+
+
+module.exports={
+    adminRouter,
+    publicRouter
+}
